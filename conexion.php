5b49f45300c2bec953bba7576a6fec1ed559284a
JaiderRomo <rupertogilrolo@gmail.com>
2025-05-12 16:00:24 -0500

    cambio a conexion 5

--- conexion.php
+++ conexion.php
@@ -1,13 +1,13 @@
 <?php
-$host = 'mysql.railway.internal';
-$port = 3306;
+$host = 'metro.proxy.rlwy.net';
+$port = 29819;
 $db   = 'railway';
 $user = 'root';
 $pass = 'GpbdPRfYHNYmGfTNcAgvbKVDccYBQCiC'; 
 $charset = 'utf8mb4';
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db;charset=$charset", $user, $pass);
+    $pdo = new PDO("mysql:host=$host;port=$port;dbname=$db;charset=$charset", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
